761dce1cbf224a0cb09032d35e1bbb491242e702
Chris <cr1s@hotmail.it>
2026-03-12 15:16:30 +0100

    All

--- src/main/kotlinCorso/Esercizio15.kt
+++ src/main/kotlinCorso/Esercizio15.kt
@@ -1,43 +1,63 @@
- interface Smartphone{
+interface Smartphone{
     fun takePhoto(){
         println("Scatto lo smartphone")
     }
 }
 
 
- interface Camera{
+interface Camera{
     fun takePhoto(){
         println("Scatto con fotocamera")
     }
 }
 
- class Iphone: SmartPhone(), Camera{
+class Iphone: Smartphone, Camera{
     override fun takePhoto(){
         super<Camera>.takePhoto()
-        super<SmartPhone>.takePhoto()
+        super<Smartphone>.takePhoto()
     }
 }
 
- //ese2---------------
- class Car{
-     var brand = ""
-     var model = ""
-     init {
-         println("Creata nuova auto: $brand $model")
-     }
- }
+//ese2---------------
+class Car{
+    var brand = ""
+    var model = ""
+    init {
+        println("Creata nuova auto: $brand $model")
+    }
+}
 
- fun main() {
-     //ese1------
-     val iphone = Iphone()
-     iphone.takePhoto()
+fun main() {
+    //ese1------
+    val iphone = Iphone()
+    iphone.takePhoto()
 
-     //ese2------
-     val car = Car().apply{
-         brand = "Ford"
-         model = "Mustang"
-     }
- }
+    //ese2------
+    val car = Car().apply{
+        brand = "Ford"
+        model = "Mustang"
+    }
+
+    val nullable:String? = "kotlin"
+    nullable?.let{
+        println(it.uppercase())
+    }
+
+    //ese3-------
+    fun operazioneSuNumeri(a: Int, b: Int, operazione: (Int, Int) -> Int): Int {
+        return operazione(a, b)
+    }
+
+    val risultato = operazioneSuNumeri(4,5){
+            a,b -> a*b
+    }
+    println(risultato)
+
+    val voti = listOf(18,25,30,15)
+
+    val votiValidi = voti.filter{it >=19}
+    println(votiValidi)
+}
 
 
 
