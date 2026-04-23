854cbc0112c0f86e3a1fd25375a1f54443e21f18
Chris <cr1s@hotmail.it>
2026-04-23 16:53:36 +0200

    add day4_extension_function with more extended fuction

--- eserciziKOTLIN/src/main/kotlin/nozionKotlin/Day4_Extension_Function.kt
+++ eserciziKOTLIN/src/main/kotlin/nozionKotlin/Day4_Extension_Function.kt
@@ -1,2 +1,20 @@
 package nozionKotlin
 
+fun main() {
+
+    println("Hello chris".appeendNewDev(" new developer!"))
+    println("this is funn".removeFirstAndLastChars())
+
+}
+
+/**
+ * Possiamo creare nuove funzioni di classi già esistenti alloccorenza
+ * si chiama Extension Function
+ * La classe String, non ha il metodo appendNewDev()
+ * in questo modo possiamo creare le nostre nuove funzioni che ci servono
+ */
+fun String.appeendNewDev(toApped:String):String = this.plus(toApped)
+
+fun String.removeFirstAndLastChars():String{
+    return this.substring(1,this.length - 1)
+}
